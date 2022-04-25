2c2f3fa077984083ff90d04c53864a9f9f334f19
Tej Patel <tejpatel317@gmail.com>
2022-04-25 16:10:11 -0400

    commit tree ea4765bf8dcf0e0413cbdebd5b1c067088289031

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1=2;
+let num2=31;
+let num3=14;
+let num4=10;
 
-// Write your code here
+const multiply = (num1 * num2);
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num3%num4;
+
+const max = Math.max(12, 20, 5, 7, 3);
